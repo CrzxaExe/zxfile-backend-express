21e137bb30f2b370b639c1915c011c147f1028d5
CrzxaExe <craserchannel@outlook.co.id>
2026-06-20 19:34:05 +0700

    fix: CORS again

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,15 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: ALLOWED_ORIGINS,
+    origin: (origin, callback) => {
+      if (!origin || ALLOWED_ORIGINS.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
